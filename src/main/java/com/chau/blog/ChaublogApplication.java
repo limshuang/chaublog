3bd0dc0cd685df2859c00aeba5ef4a7a9a423125
chau <454253985@qq.com>
2019-07-17 10:03:01 +0800

    create project.

---
+++ src/main/java/com/chau/blog/ChaublogApplication.java
@@ -0,0 +1,13 @@
+package com.chau.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaublogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaublogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chau/blog/ChaublogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chau.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChaublogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
