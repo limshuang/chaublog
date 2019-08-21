10c28c30be827d1c4215201035fd1ad18e24cc08
admin <454253985@qq.com>
2019-08-21 10:44:50 +0800

    update.

--- src/main/java/com/chau/blog/service/impl/UssAdminServiceImpl.java
+++ src/main/java/com/chau/blog/service/impl/UssAdminServiceImpl.java
@@ -39,6 +39,12 @@ public class UssAdminServiceImpl implements UssAdminService {
 
     @Override
     public UssAdmin getAdminByUsername(String username) {
+//        UmsAdminExample example = new UmsAdminExample();
+//        example.createCriteria().andUsernameEqualTo(username);
+//        List<UmsAdmin> adminList = adminMapper.selectByExample(example);
+//        if (adminList != null && adminList.size() > 0) {
+//            return adminList.get(0);
+//        }
         return null;
     }
 
